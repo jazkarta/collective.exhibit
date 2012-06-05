aafd7ad454a1e85746fc5a2fb22c99723159a149
Alec Mitchell <alecpm@gmail.com>
2012-06-05 08:20:33 -0700

    Create optional faceted search page for exhibits.

--- collective/exhibit/setuphandlers.py
+++ collective/exhibit/setuphandlers.py
@@ -1,7 +1,18 @@
+from zope.interface import alsoProvides
+from zope.event import notify
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import _createObjectByType
+from Products.ATContentTypes.lib import constraintypes
 
 from collective.exhibit import exhibitMessageFactory as _
+from config import EXHIBIT_TEMPLATES
 
+try:
+    from eea.facetednavigation.settings.interfaces import IHidePloneRightColumn
+    from eea.facetednavigation.events import FacetedGlobalSettingsChangedEvent
+    HAS_EEA = True
+except ImportError:
+    HAS_EEA = False
 
 def configureExhibitsFolder(portal):
     folder = portal.restrictedTraverse('portal-exhibit-templates')
@@ -17,3 +28,33 @@ def exhibitSetup(context):
     else:
         portal = getToolByName(context, 'portal_url').getPortalObject()
     configureExhibitsFolder(portal)
+
+def faceted_navigation(context):
+    xmlconfig = context.readDataFile('exhibit_facets.xml')
+    if xmlconfig is None or not HAS_EEA:
+        return
+    portal = context.getSite()
+    qitool = getToolByName(portal, "portal_quickinstaller")
+
+    if qitool.isProductInstallable('eea.facetednavigation'):
+        if not qitool.isProductInstalled('eea.facetednavigation'):
+            qitool.installProduct('eea.facetednavigation')
+
+        folder = portal.restrictedTraverse(EXHIBIT_TEMPLATES)
+        existing = folder.keys()
+        if 'explore-exhibit' not in existing:
+            _createObjectByType('Folder', folder, id='explore-exhibit',
+                                title=_(u'Explore Exhibit'))
+            faceted = folder['explore-exhibit']
+            alsoProvides(faceted, IHidePloneRightColumn)
+            # We need to constrain types in order to allow an ATCT
+            # folder inside a dexterity folder!
+            faceted.setConstrainTypesMode(constraintypes.ENABLED)
+            faceted.setLocallyAllowedTypes([])
+            faceted.setImmediatelyAddableTypes([])
+            # Enable faceted search view
+            faceted.unrestrictedTraverse('@@faceted_subtyper/enable')()
+            # Load default facet config/layout
+            importer = faceted.unrestrictedTraverse('@@faceted_exportimport')
+            importer.import_xml(import_file=xmlconfig, redirect=None)
+            notify(FacetedGlobalSettingsChangedEvent(faceted))
