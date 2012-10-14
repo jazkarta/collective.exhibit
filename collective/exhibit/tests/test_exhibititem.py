4942e0d1f0efe2ba2f40231a88ec23cb0c112234
Alec Mitchell <alecpm@gmail.com>
2012-10-14 16:13:18 +0200

    Add some exhibit reference tests

--- collective/exhibit/tests/test_exhibititem.py
+++ collective/exhibit/tests/test_exhibititem.py
@@ -5,6 +5,7 @@ from zope.component import createObject
 from zope.component import queryUtility
 
 from plone.dexterity.interfaces import IDexterityFTI
+from plone.uuid.interfaces import IUUID
 
 from collective.exhibit.testing import COLLECTIVE_EXHIBIT_INTEGRATION_TESTING
 
@@ -19,6 +20,22 @@ class ExhibitTest(unittest.TestCase):
         self.portal = self.layer['portal']
         self.request = self.layer['request']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def _setup_item_with_reference(self):
+        self.portal.invokeFactory('collective.exhibit.exhibitsection',
+                                  'section-a')
+        section = self.portal['section-a']
+        section.invokeFactory('collective.exhibit.exhibititem',
+                                  'item-a')
+        item = section['item-a']
+        # create item for reference
+        self.portal.invokeFactory('News Item', 'some-news', title='Some News',
+                                  description='Original Description',
+                                  text='<p>Original Text</p>',
+                                  subject=['Original', 'Keywords'])
+        referenced = self.portal['some-news']
+        item.referenced_item = IUUID(referenced)
+        return item, referenced
 
     def test_schema(self):
         fti = queryUtility(IDexterityFTI,
@@ -72,6 +89,35 @@ class ExhibitTest(unittest.TestCase):
 
         self.failUnless(view())
         self.assertEquals(view.request.response.status, 200)
+
+    def test_exhibit_reference_property_acquisition(self):
+        self.portal.invokeFactory('collective.exhibit.exhibitsection',
+                                  'section-a')
+        section = self.portal['section-a']
+        section.invokeFactory('collective.exhibit.exhibititem',
+                                  'item-a')
+        item = section['item-a']
+        # create item for reference
+        self.portal.invokeFactory('News Item', 'some-news', title='Some News',
+                                  description='Original Description',
+                                  text='<p>Original Text</p>',
+                                  subject=('Original', 'Keywords'))
+        item.referenced_item = IUUID(self.portal['some-news'])
+        # create item with reference
+        self.assertEquals(item.Title(), 'Some News')
+        self.assertEquals(item.Description(), 'Original Description')
+        self.assertEquals(item.getText(), '<p>Original Text</p>')
+        self.assertEquals(item.Subject(), ('Original', 'Keywords'))
+
+    def test_exhibit_reference_url(self):
+        item, referenced = self._setup_item_with_reference()
+        self.assertEquals(referenced.absolute_url(), item.referenced_item_url)
+
+    def test_exhibit_override_reference_title(self):
+        item, referenced = self._setup_item_with_reference()
+        self.assertEquals(item.Title(), referenced.Title())
+        item.title = u'My New Title'
+        self.assertEquals(item.Title(), u'My New Title')
 
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
